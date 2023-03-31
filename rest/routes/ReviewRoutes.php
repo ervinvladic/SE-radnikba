6a4f5d6a87023b701ed920b052139ff0e21be011
Ervin Vladić <ervin.vladic@stu.ibu.edu.ba>
2023-03-31 17:42:35 +0200

    add ReviewRoutes and UserRoutes

---
+++ rest/routes/ReviewRoutes.php
@@ -0,0 +1,26 @@
+<?php
+// CRUD operations for review entity
+
+
+Flight::route('GET /review', function(){
+  Flight::json(Flight::reviewService()->get_all());
+});
+
+
+Flight::route('GET /review/@id', function($id){
+  Flight::json(Flight::reviewService()->get_by_id($id));
+});
+
+
+Flight::route('POST /review', function(){
+  $data = Flight::request()->data->getData();
+  Flight::json(Flight::reviewService()->post_review(Flight::get("user"),$data));
+});
+
+
+Flight::route('DELETE /review/@id', function($id){
+  Flight::reviewService()->delete_review(Flight::get("user"),$id);
+  Flight::json(["message" => "deleted"]);
+});
+
+?>
---
+++ rest/routes/UserRoutes.php
@@ -0,0 +1,12 @@
+<?php
+
+Flight::route('POST /login', function(){
+    
+});
+
+Flight::route('POST /register', function(){
+
+});
+
+
+?>
