63726bb24c8175b885b13457b5534bae17402922
Harun Kunovac <harun.kunovac@stu.ibu.edu.ba>
2023-05-31 20:40:43 +0200

    feat: swagger implementation

---
+++ rest/routes/ApiSpec.php
@@ -0,0 +1,13 @@
+<?php
+
+
+/**
+ * @OA\Info(title="Radnikba API Specs", version="0.2", @OA\Contact(email="harun.kunovac@stu.ibu.edu.ba", name="Harun Kunovac"))
+ * @OA\OpenApi(
+ *    @OA\Server(url="http://localhost/SE-radnikba/rest", description="Development Environment" ),
+ *    @OA\Server(url="https://radnikba-umeug.ondigitalocean.app/rest", description="Production Environment" )
+ * ),
+ * @OA\SecurityScheme(securityScheme="ApiKeyAuth", type="apiKey", in="header", name="Authorization" )
+ */
+
+?>
